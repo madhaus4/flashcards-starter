73d79860c93d27f4f9eff1133c5d335b4c1c0fb2
Maria DelSignore <maria.ad27@gmail.com>
2021-07-07 17:44:00 -0600

    Remove pseudocode

--- src/Round.js
+++ src/Round.js
@@ -39,20 +39,3 @@ class Round {
 
 
 module.exports = Round;
-
-
-// Round class takes in responses and records guesses (correct or incorrect)
-// currentCard should be the first Card in the Deck at the start of the Round
-
-// returnCurrentCard >> returns the current card being played
-
-// takeTurn >> updates turns count, evaluates guesses, gives feedback,stores ids of incorrect guess
-// --- when a guess is made,a new Turn instance is created
-// --- the turns count is updated, regardless of whether the guess is corrector incorrect
-// --- the next card become the current turn
-// --- guess is evaluated/recorded.  incorrect guesses will be stored via theid in an array of incorrectGuesses
-// --- feedback is returned regarding whether the guess is correct or incorrect
-
-// calculatePercentCorrect >> calculates and returnsthe percentage of correct guesses
-
-// endRound >> prints to the console: ‘** Round over! ** You answered <>% of the questions correctly!’
