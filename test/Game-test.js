a4fc500ed0d69d25d9876622f3efb1a4a16e9646
Maria DelSignore <maria.ad27@gmail.com>
2021-07-07 19:43:27 -0600

    Contruct test block

---
+++ test/Game-test.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const Game = require('../src/Game');
+
+describe('Game', () => {
+    let cards, deck, round, game;
+
+    beforeEach(() => {
+        deck = new Deck([cards]);
+        round = new Round(deck);
+        game = new Game();
+    });
+
+    it('should be a function', () => {
+        expect(Game).to.be.a('function');
+    });
+
+    it('should be an instance of Game', () => {
+        expect(game).to.be.an.instanceOf(Game);
+    });
+
+    it('should be able to keep track of the current round', () => {
+        expect(game.currentRound).to.deep.equal({});
+    });
+
+    it('should instanciate cards when  game starts', () => {
+        game.start(round);
+        expect(game.start).to.be.a('function');
+    });
+});
